1a49dfb5c12ce949f0779e0184ef30b7d97b5ac0
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-04 01:09:25 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song
+ 
+ attr_accessor :name, :artist, :genre
+ 
+ @@count = 0
+ @@genres = []
+ @@artists = []
+ 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  def self.count # just for class - self 
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq!
+  end
+  
+   def self.genres
+    @@genres.uniq! 
+  end
+  
+  def self.genre_count
+    genre_count = Hash.new # or array ????
+    @@genre.each do |sng|
+      genre_count[sng] += 1
+    end
+    genre_count
+  end
+  
+  def self.artist_count
+    artist_count = Hash.new
+    @@artists.each do |art|
+      artist_count[art] += 1
+  end
+  artist_count
+ end
+ 
+end
+
+# difference between unique and uniq!
+# @ or @@ using, why
+# did we use self because it is already under initialize with @@
